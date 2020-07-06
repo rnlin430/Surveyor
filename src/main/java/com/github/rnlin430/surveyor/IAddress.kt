3627d8bc88c69589c68225b4643861234f468989
rnlin <rinkarinrin411@gmail.com>
2020-07-06 23:21:57 +0900

    Update

--- src/main/java/com/github/rnlin430/surveyor/IAddress.kt
+++ src/main/java/com/github/rnlin430/surveyor/IAddress.kt
@@ -1,11 +1,5 @@
 package com.github.rnlin430.surveyor
 
 interface IAddress {
-    val name: String
-    val id: Int
-    val x: Int
-    val y: Int
-    val z: Int
-
-   fun newAdress()
+    fun getCenterLocation();
 }
--- src/main/java/com/github/rnlin430/surveyor/RectUnit.kt
+++ src/main/java/com/github/rnlin430/surveyor/RectUnit.kt
@@ -3,7 +3,17 @@ package com.github.rnlin430.surveyor
 /**
  * 矩形の領域の最小単位
  */
-class RectUnit {
+open class RectUnit(
+        val address: String,
+        val id:Int,
+        val fromX: Int,
+        val fromZ: Int,
+        val toX: Int,
+        val toZ: Int) : IAddress {
 
-
+    override fun getCenterLocation(): List<Float> {
+        val x: Float = (fromX + toX).toFloat() / 2
+        val z = (fromZ + toZ).toFloat() / 2
+        return listOf(x, z)
+    }
 }
